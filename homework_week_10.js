b298ab7c2683e8132adf1a9403c69efd8ad6e14c
juzlemmeusethisname <161974479+juzlemmeusethisname@users.noreply.github.com>
2025-10-12 05:49:17 +0800

    Update homework_week_10.js
    ori

--- homework_week_10.js
+++ homework_week_10.js
@@ -1,17 +1,6 @@
-// visualizations.js
+  var vg_spec_publisher = "homework_week_10.vg.json"; 
 
-// Load Week 9 Map visualization
-var vg_spec = "homework_week_9.vg.json";
-vegaEmbed("#map_chart", vg_spec)
-  .then(function(result) {
-    // Map loaded successfully
-  })
-  .catch(console.error);
-
-// Load Week 10 Publisher visualization
-var vg_spec_publisher = "homework_week_10.vg.json";
-vegaEmbed("#publisher_chart", vg_spec_publisher)
-  .then(function(result) {
-    // Publisher chart loaded successfully
-  })
-  .catch(console.error);
+  vegaEmbed("#publisher_chart", vg_spec_publisher)
+    .then(function(result) {
+    })
+    .catch(console.error);
